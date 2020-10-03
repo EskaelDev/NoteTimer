5185f5f4127da3bd0184490ad48efc31da9316a8
tomek <rainbowvme@gmail.com>
2020-10-03 16:43:16 +0200

    works with mkvmerge

---
+++ MkvManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace NoteTimer
+{
+    public class MkvManager
+    {
+        private readonly string _input;
+        private readonly string _output;
+        private readonly string _chapters;
+
+        public MkvManager(string input, string output, string chapters)
+        {
+            _input = input;
+            _output = output;
+            _chapters = chapters;
+        }
+        public int Run()
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "C:\\Program Files\\MKVToolNix\\mkvmerge.exe";
+            proc.StartInfo.Arguments = string.Format("--chapters \"{0}\" -o \"{1}\" \"{2}\"", _chapters, _output, _input);
+            proc.Start();
+            proc.WaitForExit();
+            int exitCode = proc.ExitCode;
+            proc.Close();
+
+            return exitCode;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading;
 
 namespace NoteTimer
@@ -10,19 +12,26 @@ namespace NoteTimer
 
         static void Main(string[] args)
         {
-            string[] options = {"Technologie wsp.wytw.oprogr.",
-            "Inżynieria pozyskiwania i ochr",
-            "Bezpieczeństwo sys.web.i mob.",
-            "Projektowanie sys. informat.",
-            "Podstawy biz. i ochr.wł.intel.",
-            "Zast.rozw.chmur.aplik.webowych",
-            "Zaawansowane sys. baz danych"};
+            List<string> options = ReadSubjects("subjects.txt");
+            //string[] options = {"Technologie wsp.wytw.oprogr.",
+            //"Inżynieria pozyskiwania i ochr",
+            //"Bezpieczeństwo sys.web.i mob.",
+            //"Projektowanie sys. informat.",
+            //"Podstawy biz. i ochr.wł.intel.",
+            //"Zast.rozw.chmur.aplik.webowych",
+            //"Zaawansowane sys. baz danych"};
+
 
             Menu menu = new Menu();
-            int selectedSubject = menu.MultipleChoice(true, options);
-            Console.WriteLine();
+            int selectedSubject = menu.MultipleChoice(true, options.ToArray());
+            int currentChapterNumber = 1;
+
             string formatted = "";
+            Console.WriteLine("\n\n");
+
+
             Stopwatch stopwatch = new Stopwatch();
+
             while (true)
             {
                 stopwatch.Start();
@@ -35,24 +44,55 @@ namespace NoteTimer
                     break;
 
                 // format and display the timespan value.
-                formatted += FormatNote(stopwatch.Elapsed, note);
+                formatted += FormatNote(stopwatch.Elapsed, note, currentChapterNumber++);
                 Console.WriteLine(formatted);
 
             }
-            SaveFile(options[selectedSubject], formatted);
 
+            string chaptersFile = SaveFile(options[selectedSubject], formatted);
+            string inputFile = DateTime.Now.ToString("yyyy-MM-dd HH") + ".mkv";
+            string outputFile = string.Format("{0}{1}-chapterized.mkv", options[selectedSubject], inputFile.Substring(0, inputFile.Length - 4));
+
+            MkvManager manager = new MkvManager(inputFile, outputFile, chaptersFile);
+            int managerExitCode = manager.Run();
+            Console.WriteLine("Manager exited with code: ", managerExitCode);
+
+            Console.ReadLine();
         }
 
 
-        static string FormatNote(TimeSpan elapsed, string note)
+        static string FormatNote(TimeSpan elapsed, string note, int chapterNumber)
         {
-            return String.Format("{0:00}:{1:00}.{2:00} - {3}\n", elapsed.Minutes, elapsed.Seconds, elapsed.Milliseconds / 10, note);
+            return string.Format("CHAPTER{0:00}={1:00}:{2:00}:{3:00}.{4:000}\nCHAPTER{0:00}NAME={5}\n",
+                chapterNumber,
+                elapsed.Hours,
+                elapsed.Minutes,
+                elapsed.Seconds,
+                elapsed.Milliseconds / 10,
+                note);
+
         }
 
-        static void SaveFile(string title, string toSave)
+        static string SaveFile(string title, string toSave)
         {
-            string path = Path.Combine("..\\", title + " " + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".txt").ToString();
+            string path = string.Format("{0} {1}.txt", title, DateTime.Now.ToString("yyyy-MM-dd HH")).ToString();
             File.WriteAllText(path, toSave);
+            return path;
+        }
+
+        static List<string> ReadSubjects(string path)
+        {
+            string line;
+            List<string> subjects = new List<string>();
+
+            using (StreamReader file = new StreamReader(path))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    subjects.Add(line);
+                }
+            }
+            return subjects;
         }
     }
 }
